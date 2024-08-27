159224538b82862eaa3964aa4451c46b02bc76e3
Benoit Thieurmel <bthieurmel@gaia.quadratic-labs.com>
2024-08-27 15:34:57 +0000

    Init

---
+++ inst/assets/bindEnter.js
@@ -0,0 +1,34 @@
+
+
+function bindEnter(ns) {
+  $('#' + ns + 'user_pwd').on('keyup',function(e) {
+    if(e.which == 13) {
+      console.info($('#' + ns + 'user_pwd').val());
+      $('#' + ns + 'go_auth').click();
+    }
+  });
+  $('#' + ns + 'user_id').on('keyup',function(e) {
+    if(e.which == 13) {
+      $('#' + ns + 'go_auth').click();
+    }
+  });
+  
+  $('#' + ns + 'pwd_one').on('keyup',function(e) {
+    if(e.which == 13) {
+      $('#' + ns + 'update_pwd').click();
+    }
+  });
+  $('#' + ns + 'pwd_two').on('keyup',function(e) {
+    if(e.which == 13) {
+      $('#' + ns + 'update_pwd').click();
+    }
+  });
+}
+
+Shiny.addCustomMessageHandler('focus_input', function(data) {
+  $('#' + data.inputId).focus();
+});
+
+Shiny.addCustomMessageHandler('update_auth_title', function(data) {
+  $('#' + data.inputId).html(data.title);
+});
